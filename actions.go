28c6975af21583b17e5615ab50155aa8ace64b2e
Tom Fleet <tom.fleet@chetwood.co>
2025-01-16 21:13:18 +0000

    Add `Input` and `InputBool`

--- actions.go
+++ actions.go
@@ -1,7 +1,44 @@
-// Package actions is a placeholder for something cool.
+// Package actions provides a toolkit for writing GitHub Actions in Go.
 package actions
 
-// Hello returns a welcome message for the project.
-func Hello() string {
-	return "Hello actions"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Input gets the value of an actions input variable.
+//
+// It returns the value of the variable (stripped of leading or trailing whitespace)
+// and a boolean which indicates whether it was defined.
+func Input(name string) (value string, ok bool) {
+	cleaned := strings.ReplaceAll(name, " ", "_")
+	envName := fmt.Sprintf("INPUT_%s", strings.ToUpper(cleaned))
+
+	value, ok = os.LookupEnv(envName)
+	return strings.TrimSpace(value), ok
+}
+
+// InputBool gets the boolean value of an actions input variable.
+//
+// Specifically InputBool supports:
+//
+//	true | True | TRUE | false | False | FALSE
+//
+// If the variable is not defined, or if the value is not in the
+// supported list, an error is returned.
+func InputBool(name string) (bool, error) {
+	value, ok := Input(name)
+	if !ok {
+		return false, fmt.Errorf("input variable %s not defined", name)
+	}
+
+	switch value {
+	case "true", "True", "TRUE":
+		return true, nil
+	case "false", "False", "FALSE":
+		return false, nil
+	default:
+		return false, fmt.Errorf("input variable %s is invalid bool: %q", name, value)
+	}
 }
--- actions_test.go
+++ actions_test.go
@@ -4,13 +4,182 @@ import (
 	"testing"
 
 	"github.com/FollowTheProcess/actions"
+	"github.com/FollowTheProcess/test"
 )
 
-func TestHello(t *testing.T) {
-	got := actions.Hello()
-	want := "Hello actions"
+func TestInput(t *testing.T) {
+	tests := []struct {
+		env   map[string]string // Env vars to set for the test
+		name  string            // Name of the test case
+		input string            // The name of the input variable to get
+		want  string            // Expected return value
+		ok    bool              // Expected ok value
+	}{
+		{
+			name:  "empty",
+			env:   map[string]string{},
+			input: "something",
+			want:  "",
+			ok:    false,
+		},
+		{
+			name: "missing",
+			env: map[string]string{
+				"INPUT_SOMETHING": "here",
+			},
+			input: "something_else", // Would be looking for INPUT_SOMETHING_ELSE
+			want:  "",
+			ok:    false,
+		},
+		{
+			name: "found",
+			env: map[string]string{
+				"INPUT_SOMETHING": "here",
+			},
+			input: "something",
+			want:  "here",
+			ok:    true,
+		},
+		{
+			name: "found with space",
+			env: map[string]string{
+				"INPUT_SOMETHING_ELSE": "here",
+			},
+			input: "something else",
+			want:  "here",
+			ok:    true,
+		},
+		{
+			name: "return trimmed",
+			env: map[string]string{
+				"INPUT_TRIM_ME": "  okay   ",
+			},
+			input: "trim_me",
+			want:  "okay",
+			ok:    true,
+		},
+	}
 
-	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for key, val := range tt.env {
+				t.Setenv(key, val)
+			}
+
+			got, ok := actions.Input(tt.input)
+			test.Equal(t, ok, tt.ok, test.Context("ok return value did not match"))
+			test.Equal(t, got, tt.want, test.Context("returned input variable was wrong"))
+		})
+	}
+}
+
+func TestInputBool(t *testing.T) {
+	tests := []struct {
+		env     map[string]string // Env vars to set for the test
+		name    string            // Name of the test case
+		input   string            // Name of the input variable to get
+		want    bool              // Expected return value
+		wantErr bool              // Whether we wanted an error
+	}{
+		{
+			name:    "empty",
+			env:     map[string]string{},
+			input:   "something",
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "missing",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "true",
+			},
+			input:   "other",
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "valid true lower",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "true",
+			},
+			input:   "do_something",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "valid true title",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "True",
+			},
+			input:   "do_something",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "valid true upper",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "TRUE",
+			},
+			input:   "do_something",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "valid false lower",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "false",
+			},
+			input:   "do_something",
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "valid false title",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "False",
+			},
+			input:   "do_something",
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "valid false upper",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "FALSE",
+			},
+			input:   "do_something",
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "invalid true",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "yes",
+			},
+			input:   "do_something",
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "invalid false",
+			env: map[string]string{
+				"INPUT_DO_SOMETHING": "no",
+			},
+			input:   "do_something",
+			want:    false,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for key, val := range tt.env {
+				t.Setenv(key, val)
+			}
+
+			got, err := actions.InputBool(tt.input)
+			test.WantErr(t, err, tt.wantErr)
+			test.Equal(t, got, tt.want)
+		})
 	}
 }
