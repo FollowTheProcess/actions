619c8b648d232834f7cadd9c2dd72082ef02013d
Tom Fleet <tom.fleet@chetwood.co>
2025-01-19 09:40:16 +0000

    Add outputs and restructure (#13)
    ## Summary
    
    <!-- Describe your changes in detail here, if it closes an open issue,
    include "Closes #<issue>" -->

--- actions_test.go
+++ actions_test.go
@@ -1,5 +1,5 @@
-package env //nolint: testpackage
-// testpackage is off because we need access to envFile in here because this project
+package actions //nolint: testpackage
+// testpackage is off because we need access to envFile and outFile in here because this project
 // itself is tested on GitHub Actions, meaning we can't remove or manipulate the real
 // $GITHUB_ENV file to test things like what happens if it's not present etc. so for tests
 // we use $TEST_GITHUB_ENV, while the real code uses $GITHUB_ENV.
@@ -12,9 +12,12 @@ import (
 	"github.com/FollowTheProcess/test"
 )
 
-const testEnvName = "TEST_GITHUB_ENV"
+const (
+	testEnvName = "TEST_GITHUB_ENV"
+	testOutName = "TEST_GITHUB_OUTPUT"
+)
 
-func TestGet(t *testing.T) {
+func TestGetEnv(t *testing.T) {
 	tests := []struct {
 		name string            // Name of the test case
 		env  map[string]string // Env vars to set for the test
@@ -64,14 +67,14 @@ func TestGet(t *testing.T) {
 				t.Setenv(key, val)
 			}
 
-			got, ok := Get(tt.key)
+			got, ok := GetEnv(tt.key)
 			test.Equal(t, ok, tt.ok)
 			test.Equal(t, got, tt.want)
 		})
 	}
 }
 
-func TestSet(t *testing.T) {
+func TestSetEnvValidation(t *testing.T) {
 	tests := []struct {
 		name    string // Name of the test case
 		key     string // Key to set
@@ -125,8 +128,11 @@ func TestSet(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			old := envFile
 			envFile = testEnvName
-			err := Set(tt.key, tt.value)
+			t.Cleanup(func() { envFile = old })
+
+			err := SetEnv(tt.key, tt.value)
 			test.WantErr(t, err, tt.wantErr)
 			if err != nil {
 				test.Equal(t, err.Error(), tt.errMsg)
@@ -135,16 +141,19 @@ func TestSet(t *testing.T) {
 	}
 }
 
-func TestSetFile(t *testing.T) {
+func TestSetEnv(t *testing.T) {
+	old := envFile
+	envFile = testEnvName
+	t.Cleanup(func() { envFile = old })
+
 	t.Run("exists", func(t *testing.T) {
-		envFile = testEnvName
-		tmp, err := os.CreateTemp("", "TestSetFile*")
+		tmp, err := os.CreateTemp("", "TestSetEnv*")
 		test.Ok(t, err)
 		t.Cleanup(func() { os.RemoveAll(tmp.Name()) })
 
 		t.Setenv(envFile, tmp.Name()) // Set $TEST_GITHUB_ENV to the path to our file
 
-		err = Set("SOMETHING", "value")
+		err = SetEnv("SOMETHING", "value")
 		test.Ok(t, err)
 
 		contents, err := os.ReadFile(tmp.Name())
@@ -157,8 +166,7 @@ func TestSetFile(t *testing.T) {
 		test.Equal(t, os.Getenv("SOMETHING"), "value")
 	})
 	t.Run("multiline", func(t *testing.T) {
-		envFile = testEnvName
-		tmp, err := os.CreateTemp("", "TestSetFile*")
+		tmp, err := os.CreateTemp("", "TestSetEnv*")
 		test.Ok(t, err)
 		t.Cleanup(func() { os.RemoveAll(tmp.Name()) })
 
@@ -166,7 +174,7 @@ func TestSetFile(t *testing.T) {
 
 		value := "values\nacross\nmultiple\nlines"
 
-		err = Set("MULTILINE", value)
+		err = SetEnv("MULTILINE", value)
 		test.Ok(t, err)
 
 		contents, err := os.ReadFile(tmp.Name())
@@ -180,20 +188,75 @@ func TestSetFile(t *testing.T) {
 		test.Equal(t, os.Getenv("MULTILINE"), value)
 	})
 	t.Run("unset", func(t *testing.T) {
-		envFile = testEnvName
-		tmp, err := os.CreateTemp("", "TestSetFile*")
+		tmp, err := os.CreateTemp("", "TestSetEnv*")
 		test.Ok(t, err)
 		t.Cleanup(func() { os.RemoveAll(tmp.Name()) })
 
 		// Not setting $TEST_GITHUB_ENV
-		err = Set("KEY", "value")
+		err = SetEnv("KEY", "value")
 		test.Err(t, err)
 	})
 	t.Run("set but no file", func(t *testing.T) {
-		envFile = testEnvName
 		t.Setenv(envFile, "missing") // Set $TEST_GITHUB_ENV to the path of a missing file
 
-		err := Set("CONFIG", "yes")
+		err := SetEnv("CONFIG", "yes")
+		test.Err(t, err)
+	})
+}
+
+func TestSetOutput(t *testing.T) {
+	old := outFile
+	outFile = testOutName
+	t.Cleanup(func() { outFile = old })
+
+	t.Run("exists", func(t *testing.T) {
+		tmp, err := os.CreateTemp("", "TestSetOutput*")
+		test.Ok(t, err)
+		t.Cleanup(func() { os.RemoveAll(tmp.Name()) })
+
+		t.Setenv(outFile, tmp.Name()) // Set $TEST_GITHUB_OUTPUT to the path to our file
+
+		err = SetOutput("SOMETHING", "value")
+		test.Ok(t, err)
+
+		contents, err := os.ReadFile(tmp.Name())
+		test.Ok(t, err)
+
+		hasOutputSet := bytes.Contains(contents, []byte("SOMETHING=value"))
+		test.True(t, hasOutputSet)
+	})
+	t.Run("multiline", func(t *testing.T) {
+		tmp, err := os.CreateTemp("", "TestSetOutput*")
+		test.Ok(t, err)
+		t.Cleanup(func() { os.RemoveAll(tmp.Name()) })
+
+		t.Setenv(outFile, tmp.Name()) // Set $TEST_GITHUB_OUTPUT to the path to our file
+
+		value := "values\nacross\nmultiple\nlines"
+
+		err = SetOutput("MULTILINE", value)
+		test.Ok(t, err)
+
+		contents, err := os.ReadFile(tmp.Name())
+		test.Ok(t, err)
+
+		test.True(t, bytes.Contains(contents, []byte("MULTILINE<<")))
+		test.True(t, bytes.Contains(contents, []byte("ghadelimiter_")))
+		test.True(t, bytes.Contains(contents, []byte(value)))
+	})
+	t.Run("unset", func(t *testing.T) {
+		tmp, err := os.CreateTemp("", "TestSetOutput*")
+		test.Ok(t, err)
+		t.Cleanup(func() { os.RemoveAll(tmp.Name()) })
+
+		// Not setting $TEST_GITHUB_OUTPUT
+		err = SetOutput("KEY", "value")
+		test.Err(t, err)
+	})
+	t.Run("set but no file", func(t *testing.T) {
+		t.Setenv(envFile, "missing") // Set $TEST_GITHUB_OUTPUT to the path of a missing file
+
+		err := SetOutput("CONFIG", "yes")
 		test.Err(t, err)
 	})
 }
